df6b9dddd7ec7622fb1dcbf94873fdd429e42774
Michael Madden <mikemadden42@icloud.com>
2025-06-19 20:16:14 -0500

    Fix clippy issue.

--- src/main.rs
+++ src/main.rs
@@ -438,31 +438,28 @@ fn propigate_error() {
         }
     }
 }
-
-fn data_structures() {
-    impl Segment {
-        fn new(start: Waypoint, end: Waypoint) -> Self {
-            Self { start, end }
-        }
-
-        fn distance(&self) -> f64 {
-            const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
-            let start_name = &self.start.name;
-            let end_name = &self.end.name;
-            println!("Calculating distance between {start_name} & {end_name}...");
-            let start_radians = self.start.latitude.to_radians();
-            let end_radians = self.end.latitude.to_radians();
-            let delta_latitude = (self.start.latitude - self.end.latitude).to_radians();
-            let delta_longitude = (self.start.longitude - self.end.longitude).to_radians();
-            let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2)
-                + start_radians.cos()
-                    * end_radians.cos()
-                    * f64::powi((delta_longitude / 2.0).sin(), 2);
-            let central_angle = 2.0 * inner_central_angle.sqrt().asin();
-            EARTH_RADIUS_IN_KILOMETERS * central_angle
-        }
+impl Segment {
+    fn new(start: Waypoint, end: Waypoint) -> Self {
+        Self { start, end }
     }
 
+    fn distance(&self) -> f64 {
+        const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
+        let start_name = &self.start.name;
+        let end_name = &self.end.name;
+        println!("Calculating distance between {start_name} & {end_name}...");
+        let start_radians = self.start.latitude.to_radians();
+        let end_radians = self.end.latitude.to_radians();
+        let delta_latitude = (self.start.latitude - self.end.latitude).to_radians();
+        let delta_longitude = (self.start.longitude - self.end.longitude).to_radians();
+        let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2)
+            + start_radians.cos() * end_radians.cos() * f64::powi((delta_longitude / 2.0).sin(), 2);
+        let central_angle = 2.0 * inner_central_angle.sqrt().asin();
+        EARTH_RADIUS_IN_KILOMETERS * central_angle
+    }
+}
+
+fn data_structures() {
     let kcle = Waypoint {
         name: "KCLE".to_string(),
         latitude: 41.4075,
